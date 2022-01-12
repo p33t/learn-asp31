5179f8342ddcf5b8886c5347b09ae1a77b4e79d2
Peter <peter@example.com>
2022-01-12 14:42:44 -0800

    First controller

---
+++ api/Controller1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api
+{
+    [Route("/")]
+    public class Controller1 : Controller
+    {
+        // GET
+        public IActionResult Get()
+        {
+            return Ok("Hello from 1");
+        }
+    }
+}
